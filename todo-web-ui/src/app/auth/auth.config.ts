9876c968e09870af5dcf739f7040acd7b7f47f35
fizlrock <nirku0soft@yandex.ru>
2025-04-07 15:43:17 +0400

    issuer-url problem

--- todo-web-ui/src/app/auth/auth.config.ts
+++ todo-web-ui/src/app/auth/auth.config.ts
@@ -1,30 +1,9 @@
 import { PassedInitialConfig } from 'angular-auth-oidc-client';
 
-// export const authConfig: PassedInitialConfig = {
-//   config: {
-//     authority: 'https://login.microsoftonline.com/http://localhost:8081/realms/fizlrock/v2.0',
-//     authWellknownEndpointUrl: 'https://login.microsoftonline.com/common/v2.0',
-//     redirectUrl: window.location.origin,
-//     clientId: 'please-enter-clientId',
-//     scope: 'please-enter-scopes', // 'openid profile offline_access ' + your scopes
-//     responseType: 'code',
-//     silentRenew: true,
-//     useRefreshToken: true,
-//     maxIdTokenIatOffsetAllowedInSeconds: 600,
-//     issValidationOff: false,
-//     autoUserInfo: false,
-//     customParamsAuthRequest: {
-//       prompt: 'select_account', // login, consent
-//     },
-//   }
-// }
-
-
-
 
 export const authConfig: PassedInitialConfig = {
   config: {
-    authority: 'http://localhost:8081/realms/fizlrock', // URL твоего realm в Keycloak
+    authority: 'http://keycloak:8081/realms/fizlrock', // URL твоего realm в Keycloak
     redirectUrl: 'http://localhost:4200/callback',      // Куда перенаправить после логина
     clientId: 'todo-webapp',
     scope: 'openid profile email',                      // Запрашиваемые данные пользователя
